23a880b2ac8e1b3fcd1c02a5229cfb680ca7e44d
zhengzhiyuan <18825193994@163.com>
2016-04-24 23:12:51 +0800

    mvc hello

---
+++ mvcHello/src/main/java/com/zzy/mvc/config/WebInitializer.java
@@ -0,0 +1,29 @@
+package com.zzy.mvc.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * 替代web.xml 
+ * 
+ * @author zhengzhiyuan
+ */
+public class WebInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(MyMvcConfig.class);
+        ctx.setServletContext(servletContext);
+        
+        Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+
+}
