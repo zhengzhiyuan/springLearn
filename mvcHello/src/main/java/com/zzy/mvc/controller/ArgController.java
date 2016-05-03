041acab1ac059ee230f30d91dd9f8d3d6a2a23ef
zhengzhiyuan <zhengzhiyuan@gomo.com>
2016-05-03 13:06:10 +0800

    @ModelAttribute 学习例子

---
+++ mvcHello/src/main/java/com/zzy/mvc/controller/ArgController.java
@@ -0,0 +1,25 @@
+package com.zzy.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/arg")
+public class ArgController {
+
+    @ModelAttribute
+    private ArgForm addArgForm() {
+        ArgForm argForm = new ArgForm();
+        argForm.setOther("ModelAttribute init");
+        return argForm;
+    }
+
+    // 测试uri http://localhost:8080/mvcHello/eg/arg/testModel/2?name=parm&other=what
+    @RequestMapping("/testModel/{id}")
+    @ResponseBody
+    public ArgForm testArg(@ModelAttribute ArgForm argForm) {
+        return argForm;
+    }
+}
---
+++ mvcHello/src/main/java/com/zzy/mvc/controller/ArgForm.java
@@ -0,0 +1,44 @@
+package com.zzy.mvc.controller;
+
+/**
+ * arg请求过来的对象
+ *
+ * @author zhengzhiyuan
+ * @since May 3, 2016
+ */
+public class ArgForm {
+    private Integer id;
+    private String name;
+    private String other;
+
+    public ArgForm() {
+        this.id = 1;
+        this.name = "name";
+        this.other = "other info";
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOther() {
+        return other;
+    }
+
+    public void setOther(String other) {
+        this.other = other;
+    }
+
+}
